dd7e532540125e1bf88048a2f4c4eff551e379e3
xijunfei <xijunfei@ema.com>
2018-07-06 10:44:07 +0800

    111

---
+++ Assets/Rua/RuaCommand.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RuaCommand {
+	private RuaContext context;
+	public RuaCommand(string code) {
+		
+	}
+
+	public RuaCommand() {
+
+	}
+
+	public virtual RuaCommand Make(RuaContext context, string code) {
+		if (code.Contains ("=")) {
+			RuaCommandSet rua = new RuaCommandSet (code);
+			rua.context = context;
+			return rua;
+		}
+		return null;
+//		return new RuaCommandNormal (code);
+	}
+}
+
+public class RuaCommandNormal {
+	public RuaCommandNormal(string code){
+		
+	}
+}
+
+public class RuaCommandSet : RuaCommand {
+	private RuaObject Left;
+	private RuaObject Right;
+	public RuaCommandSet(string code){
+		string[] values = code.Split ('=');
+	}
+	public RuaCommandSet(){
+		
+	}
+}
--- Assets/Rua/Test.cs
+++ Assets/Rua/Test.cs
@@ -8,21 +8,75 @@ public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		RuaContext.ParseText ("var sss=1;");
-		RuaContext.ParseText ("aaa.ccc().ttt2();");
-		RuaContext.ParseText ("aaa.bbb(aaa.qqq.ddd());");
+//		RuaContext.ParseText ("var sss=1;");
+//		RuaContext.ParseText ("aaa.ccc().ttt2();");
+//		RuaContext.ParseText ("aaa.bbb(aaa.qqq.ddd());");
 //		aaa.ttt ();
 //		RuaContext.ParseText ("aaa.bbb(aaa.qqq.ddd());");
 //		RuaFunctionAddComponent add = new RuaFunctionAddComponent ();
 //		add.gameObject = this.gameObject;
 //		add.Excute ("Test");
-
+//		Debug.LogError(LengthOfLongestSubstring("cdd"));
+		int[] aaa = new int[]{ 1, 2, 2 };
+		RemoveDuplicates (aaa);
+		for (int i = 0; i < aaa.Length; i++) {
+			Debug.LogError (aaa [i]);
+		}
 	}
 
 	void Update () {
 		
 	}
+
+	public int RemoveDuplicates(int[] nums) {
+		if (nums.Length == 0) {
+			return 0;
+		}
+		int length = 1;
+		int lastNumber = nums [0];
+		for (int i = 1; i < nums.Length; i++) {
+			int num_i = nums [i];
+			if (num_i != lastNumber) {
+				lastNumber = num_i;
+				nums [length] = num_i;
+				length++;
+			}
+		}
+		return length;
+	}
+
+	public int LengthOfLongestSubstring(string s) {
+		int currentIndex = 0;
+		int maxLength = 0;
+		int currentLength = 0;
+		int offset = 0;
+		for (int i = 0; i < s.Length; i++) {
+			bool same = false;
+			for (int j = currentIndex; j < currentIndex + currentLength; j++) {
+				if (s[j] == s[i]) {
+					offset = j - currentIndex + 1;
+					same = true;
+					break;
+				}
+			}
+			if (same) {
+				if (currentLength > maxLength) {
+					maxLength = currentLength;
+				}
+				currentIndex += offset;
+				currentLength = currentLength - offset + 1;
+			}
+			else {
+				currentLength++;
+			}
+		}
+		if (currentLength > maxLength) {
+			maxLength = currentLength;
+		}
+		return maxLength;
+	}
 }
+
 
 public class aaa {
 	public static ccc qqq = new ccc();
